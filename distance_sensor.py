e5714432191e6c7628423053ffb47a3457585a92
Edgar Milla <edgar.milla#gmail.com>
2023-01-24 19:54:15 -0800

    changed start time as per comment on tutorial

--- distance_sensor.py
+++ distance_sensor.py
@@ -21,9 +21,11 @@ try:
         GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
         while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
+            pass
+        pulse_start_time = time.time()
         while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
+            pass
+        pulse_end_time = time.time()
 
         pulse_duration = pulse_end_time - pulse_start_time
 
