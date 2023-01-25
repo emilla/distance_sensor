c68e7629634096a2746b1b4b9df9972caa3c75da
Edgar Milla <edgar.milla#gmail.com>
2023-01-24 19:49:05 -0800

    Made it so that it constantly measures distance

--- distance_sensor.py
+++ distance_sensor.py
@@ -10,24 +10,24 @@ try:
 
     GPIO.setup(PIN_TRIGGER, GPIO.OUT)
     GPIO.setup(PIN_ECHO, GPIO.IN)
-    
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
-    print("Waiting for sensor to settle")
-    time.sleep(2)
+    while(True):
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        print("Waiting for sensor to settle")
+        time.sleep(2)
 
-    print("Calculating distance")
-    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-    time.sleep(0.00001)
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        print("Calculating distance")
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-    while GPIO.input(PIN_ECHO)==0:
-        pulse_start_time = time.time()
-    while GPIO.input(PIN_ECHO)==1:
-        pulse_end_time = time.time()
+        while GPIO.input(PIN_ECHO)==0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO)==1:
+            pulse_end_time = time.time()
 
-    pulse_duration = pulse_end_time - pulse_start_time
+        pulse_duration = pulse_end_time - pulse_start_time
 
-    distance = round(pulse_duration * 17150, 2)
-    print("Distance: ",distance, " cm")
+        distance = round(pulse_duration * 17150, 2)
+        print("Distance: ",distance, " cm")
 finally:
     GPIO.cleanup()
